5f7c4bc759f81242bda27e2f8426afc06ca5e712
suspinlaj <susprogramm@gmail.com>
2025-10-21 09:51:48 +0200

    Pantalla Inicio

--- app/src/main/java/com/example/recetas_susanapineros/MainActivity.kt
+++ app/src/main/java/com/example/recetas_susanapineros/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.recetas_susanapineros
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.recetas_susanapineros.databinding.ActivityMainBinding
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    fun onClickRecetas(vista : View) {
+        val intent = Intent(this, Progreso::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickIngredientes(vista : View) {
+        val intent = Intent(this, Ingredientes::class.java)
+        startActivity(intent)
     }
 }
